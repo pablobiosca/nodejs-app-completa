88950691fe082d70ae87a66f0ed2e471873a1443
chabaking <zaslake@gmail.com>
2023-02-14 14:50:47 +0100

    conexion a bd

---
+++ database.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2/promise")
+
+const { database } = require("./keys")
+
+const pool = mysql.createPool(database)
+
+
+
+module.exports = pool
--- routes/links.js
+++ routes/links.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const pool = require("../database")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('estas en links!!!')
+router.get('/', async function(req, res, next) {
+  const [result] = await pool.query("select 1+1")
+  res.json(result)
 });
 
 module.exports = router;
