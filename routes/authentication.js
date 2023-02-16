7db21070cc58743d54b4be991168ef079d89e920
chabaking <zaslake@gmail.com>
2023-02-16 15:29:56 +0100

    ruta login + vista

--- routes/authentication.js
+++ routes/authentication.js
@@ -13,6 +13,14 @@ router.post('/signup', passport.authenticate("local.signup",  {
     failureFlash:true
 }))
 
+router.get("/signin",(req,res)=>{
+  res.render("auth/signin")
+})
+
+router.post("/signin",(req,res)=>{
+  res.send("datos recibidos")
+})
+
 router.get("/profile",(req,res)=>{
   res.send("profile")
 })
